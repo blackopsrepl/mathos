2eea97eaf7c6674eafac761c9bb0a409c6856c92
Vittorio Distefano <info@vdistefano.studio>
2024-06-22 17:09:42 +0200

    feat(tests): add new test files and initial setup for main tests
    - added `test_ArchetypeFactory.py`, `test_EssayPlannerFactory.py` and `test_main.py` in the tests/src/factory directory
    - imported pytest and respective modules in each file
    - initiated parameterized testing setup for `run_chain` function in `test_main.py`

---
+++ tests/src/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+
+import src.main
+
+
+@pytest.mark.parametrize(
+    "date_range,expected",
+    [
+        # TODO: fill in test data for test_run_chain
+        # pytest.param(, id="")
+    ]
+)
+def test_run_chain(date_range, expected):
+    # TODO: create assertions for test_run_chain
+    # src.main.run_chain(date_range)
+    pass
